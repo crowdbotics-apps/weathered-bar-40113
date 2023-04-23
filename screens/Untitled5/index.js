b5d3b9d80d9aa3b0b77da3e9a3e80b89063bb64d
Diyi Xiang <royxiang1@gmail.com>
2023-04-22 23:04:53 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,3 +1,4 @@
+import { ImageBackground } from "react-native";
 import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 
 const ScreenComponent = () => {
@@ -5,10 +6,10 @@ const ScreenComponent = () => {
       <View style={styles.row}>
         <View style={styles.leftColumn}>
           <Image source={require("./rebeccarhodes2.png")} style={styles.image} />
-          <Text style={styles.title}>Product Title</Text>
+          <Text style={styles.title}>A01</Text>
           <Text style={styles.description}>Product Description</Text>
           <Text style={styles.price}>$9.99</Text>
-        </View>
+        <ImageBackground style={styles.bKMcKlGo} source={require("./rebeccarhodes2.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.YInrWBPk} source={require("./rebeccarhodes2.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.bRcVuqKa} source={require("./rebeccarhodes2.png")} resizeMode="cover"></ImageBackground></View>
         <View style={styles.rightColumn}>
           <Text style={styles.calendar}>Calendar</Text>
         </View>
@@ -49,30 +50,61 @@ const styles = StyleSheet.create({
     height: 597
   },
   image: {
-    width: 388,
-    height: 489,
+    width: 232,
+    height: 267,
     resizeMode: "cover",
     marginBottom: 10,
-    position: "relative",
-    left: 0
+    position: "absolute",
+    left: 1,
+    top: -5
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 5
+    marginBottom: 5,
+    position: "absolute",
+    left: 12,
+    top: 553
   },
   description: {
     fontSize: 16,
-    marginBottom: 5
+    marginBottom: 5,
+    position: "absolute",
+    left: 10,
+    top: 581
   },
   price: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    position: "absolute",
+    left: 9,
+    top: 612
   },
   calendar: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#666'
+  },
+  bKMcKlGo: {
+    width: 229,
+    height: 266,
+    position: "absolute",
+    left: 316,
+    top: -2
+  },
+  YInrWBPk: {
+    width: 222,
+    height: 244,
+    position: "absolute",
+    left: 4,
+    top: 294
+  },
+  bRcVuqKa: {
+    width: 223,
+    height: 242,
+    position: "absolute",
+    left: 317,
+    top: 294
   }
 });
 export default ScreenComponent;
